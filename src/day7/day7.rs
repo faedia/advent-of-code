a43415dfb32195c799f6e26761044296e1473e8c
Emma Tomlinson <t97emma@gmail.com>
2024-04-15 16:51:29 +0100

    Don't copy the strings, just use slices

--- src/day7/day7.rs
+++ src/day7/day7.rs
@@ -5,59 +5,57 @@ use std::{
 };
 
 #[derive(Debug, PartialEq, Clone)]
-enum Command {
+enum Command<'a> {
     AssignLit(u16),
-    AssignVar(String),
-    And(String, String),
-    Or(String, String),
-    Not(String),
-    LShift(String, u16),
-    RShift(String, u16),
+    AssignVar(&'a str),
+    And(&'a str, &'a str),
+    Or(&'a str, &'a str),
+    Not(&'a str),
+    LShift(&'a str, u16),
+    RShift(&'a str, u16),
 }
 
-fn parse_command(s: &str) -> Result<(String, Command), String> {
+fn parse_command(s: &str) -> Result<(&str, Command), String> {
     let split = s.trim().split(' ').collect::<Vec<_>>();
     match split.as_slice() {
         [a, "->", b] if matches!(a.parse::<u16>(), Ok(_)) => {
-            Ok((b.to_string(), Command::AssignLit(a.parse::<u16>().unwrap())))
+            Ok((b, Command::AssignLit(a.parse::<u16>().unwrap())))
         }
-        [a, "->", b] => Ok((b.to_string(), Command::AssignVar(a.to_string()))),
-        [a, "AND", b, "->", c] => Ok((c.to_string(), Command::And(a.to_string(), b.to_string()))),
-        [a, "OR", b, "->", c] => Ok((c.to_string(), Command::Or(a.to_string(), b.to_string()))),
-        [a, "LSHIFT", b, "->", c] if matches!(b.parse::<u16>(), Ok(_)) => Ok((
-            c.to_string(),
-            Command::LShift(a.to_string(), b.parse::<u16>().unwrap()),
-        )),
-        [a, "RSHIFT", b, "->", c] if matches!(b.parse::<u16>(), Ok(_)) => Ok((
-            c.to_string(),
-            Command::RShift(a.to_string(), b.parse::<u16>().unwrap()),
-        )),
-        ["NOT", a, "->", b] => Ok((b.to_string(), Command::Not(a.to_string()))),
+        [a, "->", b] => Ok((b, Command::AssignVar(a))),
+        [a, "AND", b, "->", c] => Ok((c, Command::And(a, b))),
+        [a, "OR", b, "->", c] => Ok((c, Command::Or(a, b))),
+        [a, "LSHIFT", b, "->", c] if matches!(b.parse::<u16>(), Ok(_)) => {
+            Ok((c, Command::LShift(a, b.parse::<u16>().unwrap())))
+        }
+        [a, "RSHIFT", b, "->", c] if matches!(b.parse::<u16>(), Ok(_)) => {
+            Ok((c, Command::RShift(a, b.parse::<u16>().unwrap())))
+        }
+        ["NOT", a, "->", b] => Ok((b, Command::Not(a))),
         _ => Err(format!("Error in {}", s)),
     }
 }
 
-fn parse(s: &str) -> Result<HashMap<String, Command>, String> {
+fn parse(s: &str) -> Result<HashMap<&str, Command>, String> {
     s.lines().map(parse_command).collect()
 }
 
-fn process_commands(commands: &HashMap<String, Command>, wires: &mut HashMap<String, u16>) {
+fn process_commands<'a>(commands: &'a HashMap<&str, Command>, wires: &mut HashMap<&'a str, u16>) {
     let mut deque = commands.keys().collect::<VecDeque<_>>();
 
     while !deque.is_empty() {
         let wire = deque.front().unwrap();
         if let Ok(n) = wire.parse::<u16>() {
-            wires.insert((*wire).clone(), n);
+            wires.insert(wire, n);
             deque.pop_front();
         } else {
             match commands.get(*wire).unwrap() {
                 Command::AssignLit(n) => {
-                    wires.insert((*wire).clone(), *n);
+                    wires.insert(wire, *n);
                     deque.pop_front();
                 }
                 Command::AssignVar(a) => {
                     if let Some(n) = wires.get(a) {
-                        wires.insert((*wire).clone(), *n);
+                        wires.insert(wire, *n);
                         deque.pop_front();
                     } else {
                         deque.push_front(a);
@@ -66,7 +64,7 @@ fn process_commands(commands: &HashMap<String, Command>, wires: &mut HashMap<Str
                 Command::And(a, b) => {
                     if let Some(n_a) = wires.get(a) {
                         if let Some(n_b) = wires.get(b) {
-                            wires.insert((*wire).clone(), n_a & n_b);
+                            wires.insert(wire, n_a & n_b);
                             deque.pop_front();
                         } else {
                             deque.push_front(b);
@@ -78,7 +76,7 @@ fn process_commands(commands: &HashMap<String, Command>, wires: &mut HashMap<Str
                 Command::Or(a, b) => {
                     if let Some(n_a) = wires.get(a) {
                         if let Some(n_b) = wires.get(b) {
-                            wires.insert((*wire).clone(), n_a | n_b);
+                            wires.insert(wire, n_a | n_b);
                             deque.pop_front();
                         } else {
                             deque.push_front(b);
@@ -89,7 +87,7 @@ fn process_commands(commands: &HashMap<String, Command>, wires: &mut HashMap<Str
                 }
                 Command::Not(a) => {
                     if let Some(n) = wires.get(a) {
-                        wires.insert((*wire).clone(), !n);
+                        wires.insert(wire, !n);
                         deque.pop_front();
                     } else {
                         deque.push_front(a);
@@ -97,7 +95,7 @@ fn process_commands(commands: &HashMap<String, Command>, wires: &mut HashMap<Str
                 }
                 Command::LShift(a, n) => {
                     if let Some(n_a) = wires.get(a) {
-                        wires.insert((*wire).clone(), n_a << n);
+                        wires.insert(wire, n_a << n);
                         deque.pop_front();
                     } else {
                         deque.push_front(a);
@@ -105,7 +103,7 @@ fn process_commands(commands: &HashMap<String, Command>, wires: &mut HashMap<Str
                 }
                 Command::RShift(a, n) => {
                     if let Some(n_a) = wires.get(a) {
-                        wires.insert((*wire).clone(), n_a >> n);
+                        wires.insert(wire, n_a >> n);
                         deque.pop_front();
                     } else {
                         deque.push_front(a);
@@ -116,21 +114,21 @@ fn process_commands(commands: &HashMap<String, Command>, wires: &mut HashMap<Str
     }
 }
 
-fn part1(commands: &HashMap<String, Command>, wire: String) -> u16 {
-    let mut wires: HashMap<String, u16> = HashMap::new();
+fn part1(commands: &HashMap<&str, Command>, wire: &str) -> u16 {
+    let mut wires: HashMap<&str, u16> = HashMap::new();
     process_commands(commands, &mut wires);
 
     *wires.get(&wire).unwrap()
 }
 
-fn part2(commands: &HashMap<String, Command>, wire: String) -> u16 {
-    let mut wires: HashMap<String, u16> = HashMap::new();
+fn part2(commands: &HashMap<&str, Command>, wire: &str) -> u16 {
+    let mut wires: HashMap<&str, u16> = HashMap::new();
     process_commands(commands, &mut wires);
 
     let new_b = *wires.get(&wire).unwrap();
 
     wires.clear();
-    wires.insert("b".to_string(), new_b);
+    wires.insert("b", new_b);
     process_commands(commands, &mut wires);
     *wires.get(&wire).unwrap()
 }
@@ -143,9 +141,9 @@ struct Cli {
 fn main() {
     let args: Cli = Cli::parse();
     let input_str = read_to_string(args.input).unwrap();
-    let part1_result = part1(&parse(&input_str).unwrap(), "a".to_string());
+    let part1_result = part1(&parse(&input_str).unwrap(), "a");
     println!("Solution for part 1: {}", part1_result);
-    let part2_result = part2(&parse(&input_str).unwrap(), "a".to_string());
+    let part2_result = part2(&parse(&input_str).unwrap(), "a");
     println!("Solution for part 2: {}", part2_result);
 }
 
@@ -156,39 +154,27 @@ mod tests {
     fn parse_command_test() {
         assert_eq!(
             super::parse_command("123 -> x"),
-            Ok((String::from("x"), super::Command::AssignLit(123)))
+            Ok(("x", super::Command::AssignLit(123)))
         );
         assert_eq!(
             super::parse_command("x AND y -> z"),
-            Ok((
-                String::from("z"),
-                super::Command::And(String::from("x"), String::from("y"))
-            ))
+            Ok(("z", super::Command::And("x", "y")))
         );
         assert_eq!(
             super::parse_command("a OR b -> c"),
-            Ok((
-                String::from("c"),
-                super::Command::Or(String::from("a"), String::from("b"))
-            ))
+            Ok(("c", super::Command::Or("a", "b")))
         );
         assert_eq!(
             super::parse_command("p LSHIFT 2 -> q"),
-            Ok((
-                String::from("q"),
-                super::Command::LShift(String::from("p"), 2)
-            ))
+            Ok((("q"), super::Command::LShift("p", 2)))
         );
         assert_eq!(
             super::parse_command("r RSHIFT 3 -> s"),
-            Ok((
-                String::from("s"),
-                super::Command::RShift(String::from("r"), 3)
-            ))
+            Ok((("s"), super::Command::RShift("r", 3)))
         );
         assert_eq!(
             super::parse_command("NOT e -> f"),
-            Ok((String::from("f"), super::Command::Not(String::from("e"))))
+            Ok((("f"), super::Command::Not("e")))
         );
     }
 
@@ -208,7 +194,8 @@ mod tests {
         );
         assert!(data.is_ok());
         let mut wires = HashMap::new();
-        super::process_commands(&data.unwrap(), &mut wires);
+        let binding = data.unwrap();
+        super::process_commands(&binding, &mut wires);
         assert_eq!(wires["d"], 72);
         assert_eq!(wires["e"], 507);
         assert_eq!(wires["f"], 492);
